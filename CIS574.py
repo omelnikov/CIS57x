6302c1b97cf301b3a8d012102622cb685f99f07e
OLEG MELNIKOV <omelnikov@users.noreply.github.com>
2021-01-21 15:48:20 -0800

    Update CIS574.py

--- CIS574.py
+++ CIS574.py
@@ -1,4 +1,5 @@
-import numpy as np, pandas as pd, nltk, re, matplotlib.pyplot as plt, wikipedia
+from IPython.core.interactiveshell import InteractiveShell as IS; IS.ast_node_interactivity = "all" # OK TO DELETE
+import numpy as np, pandas as pd, nltk, re, matplotlib.pyplot as plt, seaborn as sns
 RED, END = '\033[91m', '\033[0m'
 printRed = lambda sTxt: print(RED + sTxt + END)  # lambda function
 Assert = lambda bCond=False, sTxt='': printRed(sTxt) if bCond else None

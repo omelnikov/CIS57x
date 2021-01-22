1b65dd9c4ea21382b816e6504119338f28b35181
OLEG MELNIKOV <omelnikov@users.noreply.github.com>
2021-01-21 16:27:09 -0800

    Update CIS574.py

--- CIS574.py
+++ CIS574.py
@@ -1,3 +1,4 @@
+import sys
 from IPython.core.interactiveshell import InteractiveShell as IS; IS.ast_node_interactivity = "all" # OK TO DELETE
 import numpy as np, pandas as pd, nltk, re, matplotlib.pyplot as plt, seaborn as sns
 RED, END = '\033[91m', '\033[0m'
